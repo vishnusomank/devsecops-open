69baaae00cb3e4108d5c8a5be683ba359d9499f0
vishnusomank <vishnu@accuknox.com>
2021-06-10 18:53:57 +0530

    added php function to sanitize the input

--- vulnerabilities/ssti/home.php
+++ vulnerabilities/ssti/home.php
@@ -34,8 +34,46 @@ Web application uses templates to make the web pages look more dynamic. Template
                </div> 
             </form>
             <?php
+                //// code snipet to purify url
+                function xss_clean($data)
+                {
+                    // Fix &entity\n;
+                    $data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
+                    $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+                    $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+                    $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+                    
+                    // Remove any attribute starting with "on" or xmlns
+                    $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+                    
+                    // Remove javascript: and vbscript: protocols
+                    $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+                    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+                    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+                    
+                    // Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+                    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+                    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+                    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+                    
+                    // Remove namespaced elements (we do not need them)
+                    $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+                    
+                    do
+                    {
+                        // Remove really unwanted tags
+                        $old_data = $data;
+                        $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+                    }
+                    while ($old_data !== $data);
+                    
+                    // we are done...
+                    return $data;
+                }
+            ?>
+            <?php
                 if (isset($_GET['submit'])) {
-                    $name=$_GET['name'];
+                    $name=xss_clean(htmlspecialchars($_GET['name']));
                     // include and register Twig auto-loader
                     include 'vendor/twig/twig/lib/Twig/Autoloader.php';
                     Twig_Autoloader::register();
@@ -47,7 +85,7 @@ Web application uses templates to make the web pages look more dynamic. Template
                               $twig = new Twig_Environment($loader);
                          // set template variables
                          // render template
-                            $result= $twig->render($name);
+                            $result= xss_clean($twig->render($name));
                             echo "Hello $result";
   
                     } catch (Exception $e) {
